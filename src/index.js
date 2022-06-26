0b935b0d9f25821ed424595f95a466e48f73d7d0
Mikhail Stankevich <mikhailstn1@gmail.com>
2022-06-26 08:16:41 +0300

    feat: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function toReadable (number) {
-  
-}
+const unitsArray = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+const dozensArray = ['', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+const tensArray = ['', 'ten', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
