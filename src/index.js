7569915f61884fba0fca8ae9fabf97d448dde8b3
Mikhail Stankevich <mikhailstn1@gmail.com>
2022-06-29 03:12:51 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
-const unitsArray = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-const dozensArray = ['', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-const tensArray = ['', 'ten', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+module.exports = function toReadable(number) {
+    const unitsArray = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const dozensArray = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tensArray = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
+    let stringNumber = number.toString();
+
+    if (stringNumber.length === 1) {
+        return unitsArray[number]
+    } else if (stringNumber.length === 2) {
+        if (stringNumber[1] === '0') {
+            return tensArray[stringNumber[0] - 1]
+        } else if (stringNumber > 10 && stringNumber < 20) {
+            return dozensArray[stringNumber[1] - 1]
+        } else if (stringNumber > 20 && stringNumber < 100) {
+            return tensArray[stringNumber[0] - 1] + ' ' + unitsArray[stringNumber[1]]
+        }
+    } else if (stringNumber.length === 3) {
+        if (stringNumber[1] === '0' && stringNumber[2] === '0') {
+            return unitsArray[stringNumber[0]] + ' hundred'
+        } else if (stringNumber[1] === '0' && stringNumber[2] !== '0') {
+            return unitsArray[stringNumber[0]] + ' hundred ' + unitsArray[stringNumber[2]]
+        } else if (stringNumber[1] !== '0' && stringNumber[2] === '0') {
+            return unitsArray[stringNumber[0]] + ' hundred ' + tensArray[stringNumber[1] - 1]
+        } else if (stringNumber[1] === '1') {
+            return unitsArray[stringNumber[0]] + ' hundred ' + dozensArray[stringNumber[2] - 1]
+        } else if (stringNumber[1] !== '1' && stringNumber[1] !== '0') {
+            return unitsArray[stringNumber[0]] + ' hundred ' + tensArray[stringNumber[1] - 1] + ' ' + unitsArray[stringNumber[2]]
+        }
+    }
+}
